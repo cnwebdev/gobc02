80202f773b12f4dd09467a9311206ea7946df00e
Chris N <cn@mbp.local>
2020-11-29 23:08:29 -0800

    Adding l2 code example

---
+++ l2/mars/mars.go
@@ -0,0 +1,14 @@
+// Listing 2.1 Hello Mars: mars.go
+// My weight loss program
+package main
+
+import "fmt"
+
+// main is the function where it all begins.
+func main() {
+	fmt.Print("My weight on the surface of Mars is ")
+	fmt.Print(140.0 * .3783)
+	fmt.Print(" lbs, and I would be ")
+	fmt.Print(41 * 365 / 687)
+	fmt.Print(" years old.")
+}
